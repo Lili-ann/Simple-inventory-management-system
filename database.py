f37c3b1dc2dc14e71144c31638ed64e550a7e12a
Ngozi Lilian Okafor <114125404+Lili-ann@users.noreply.github.com>
2026-07-30 09:02:50 +0700

    modification

--- database.py
+++ database.py
@@ -1,8 +1,13 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://lili:lilian17@inventory_postgres:5432/inventory_db"
+DB_USER = os.getenv("POSTGRES_USER")
+DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+DB_NAME = os.getenv("POSTGRES_DB")
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@inventory_postgres:5432/{DB_NAME}"
 
 # Create the SQLAlchemy "engine" 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
