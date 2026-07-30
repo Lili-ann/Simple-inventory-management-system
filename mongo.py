71d1afe9067dbd6e2b21c4d21c67423898d59a2b
Ngozi Lilian Okafor <114125404+Lili-ann@users.noreply.github.com>
2026-07-30 09:03:56 +0700

    Use environment variables for MongoDB
    Refactor MongoDB connection to use environment variables for credentials.

--- mongo.py
+++ mongo.py
@@ -1,6 +1,10 @@
+import os
 from motor.motor_asyncio import AsyncIOMotorClient
 
-MONGO_URL = "mongodb://lili:lilian17@inventory_mongo:27017/"
+MONGO_USER = os.getenv("MONGO_ROOT_USERNAME")
+MONGO_PASSWORD = os.getenv("MONGO_ROOT_PASSWORD")
+
+MONGO_URL = f"mongodb://{MONGO_USER}:{MONGO_PASSWORD}@inventory_mongo:27017/"
 
 #Connect to the MongoDB server
 client = AsyncIOMotorClient(MONGO_URL)
